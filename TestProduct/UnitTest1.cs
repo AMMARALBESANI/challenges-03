06ed34b4c4c14a62818bb8fb0e29340fda81759a
Ammar <100584308+AMMARALBESANI@users.noreply.github.com>
2023-06-19 23:08:01 +0300

    all done

--- TestProduct/UnitTest1.cs
+++ TestProduct/UnitTest1.cs
@@ -7,43 +7,98 @@ namespace TestProduct
     public class UnitTest1
     {
 
-
-        [Fact]
-        public void Test1()
-        {
-            //arrang
-            string num = "1 4 5";
-
-            var numreader = new StringReader(num);
-            Console.SetIn(numreader);
-
-            //act
-            int result = Program.product();
-
-            // assert
-            Assert.Equal(20, result);
-        }
-
         [Theory]
         [InlineData(20, "1 4 5")]
         [InlineData(0, "1 4")]
         [InlineData(20, "1 4 5 6")]
         [InlineData(1, "")]
         [InlineData(20, "1 -4 5")]
-        [InlineData(1, "kk")]
+        [InlineData(1, "1 k 1")]
 
-        public void Test2(int expected, string actual)
+        public void Testprodect(int expected, string actual)
         {
             var numreader = new StringReader(actual);
             Console.SetIn(numreader);
 
             //act
-            int result = Program.product();
+            int result = Program.Product();
 
             //assert
             Assert.Equal(expected, result);
 
         }
+
+        [Theory]
+        [InlineData(4, "4\n4\n4\n4\n4 ")]
+        [InlineData(4, "4\n4\n4\n4.5\n4 ")]
+        [InlineData(4, "5\n4\n4\n3\n6\n5 ")]
+
+        public void Testaverage(double expected, string actual)
+        {
+            var numreader = new StringReader(actual);
+            Console.SetIn(numreader);
+
+            //act
+            double result = Program.average();
+
+            //assert
+            Assert.Equal(expected, result);
+
+        }
+
+        [Theory]
+        [InlineData(  1,new int[]  { 1, 1, 1, 1, 1, 1, 1 })]
+        [InlineData(   2,new int[] {1,2,2,4,5 })]
+        [InlineData(2, new int[] {2,2 ,4,4,5})]
+        [InlineData(2, new int[] { 2, 4, 5 })]
+        public void mostrebited(int expected,int[] actual )
+        {
+
+            //act
+            double result = Program.mostRebited( actual);
+
+            //assert
+            Assert.Equal(expected, result);
+
+        }
+
+        [Theory]
+        [InlineData(-3, new int[] { -3,-4,-5,-6 })]
+        [InlineData(5, new int[] { 1, 2, -2, 4, 5 })]
+        [InlineData(99, new int[] { 2, 2, 4, 4, 99 })]
+
+        public void largerNumber(int expected, int[] actual)
+        {
+
+
+            //act
+            double result = Program.largerNumber(actual);
+
+            //assert
+            Assert.Equal(expected, result);
+
+        }
+
+        
+
+        [Theory]
+        [InlineData("hello world", new string[] { "hello: 5", "world: 5" })]
+        [InlineData("yes i did it", new string[] { "yes: 3", "i: 1", "did: 3", "it: 2" })]
+        public void WordLengthArrayTest(string input, string[] expected)
+        {
+            // Arrange
+            Console.SetIn(new StringReader(input));
+
+            // Act
+            string[] result = Program.senteneslength();
+
+            // Assert
+            Assert.Equal(expected, result);
+            Assert.IsType<string[]>(result);
+        }
+
+
+
 
 
     }
--- lab-03/Program.cs
+++ lab-03/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace lab_03
+﻿using System.IO;
+using System.Transactions;
+
+namespace lab_03
 {
     public class Program
     {
@@ -6,10 +9,22 @@
         {
             try
             {
+                //Console.WriteLine(average());
+                //Console.WriteLine(Product());
+                //Drawing();
+                //Console.WriteLine(mostRebited(new int[] {5,5,6,7,8 }));
+                //Console.WriteLine(largerNumber(new int[] { 5, 5, 6, 7, 8 }));
+                //writeTofile("hello");
+                //readFromfile();
+                //deleteFromfile(Console.ReadLine());
 
-                Console.WriteLine(product());
+                string[] x = senteneslength();
+                foreach (string s in x) { Console.WriteLine(s); }
+                    
+
             }
             catch (Exception ex)
+
             {
 
                 Console.WriteLine(ex.Message);
@@ -18,13 +33,12 @@
 
         }
 
-        public static int product()
+        public static int Product()
         {
             int product = 1;
             try
             {
-
-                Console.WriteLine("please ener 3 numbers :");
+                Console.WriteLine("Please enter 3 numbers:");
                 string num = Console.ReadLine();
 
                 if (string.IsNullOrEmpty(num))
@@ -33,37 +47,37 @@
                 }
 
                 string[] numArray = num.Trim().Split(' ');
-                int[] nums = Array.ConvertAll(numArray, int.Parse);
 
-
-
+                int[] nums = new int[numArray.Length];
+                for (int i = 0; i < numArray.Length; i++)
+                {
+                    if (!int.TryParse(numArray[i], out nums[i]))
+                    {
+                        nums[i] = 1;
+                    }
+                }
 
                 if (nums.Length == 3)
                 {
-
                     for (int i = 0; i < nums.Length; i++)
-
                     {
                         if (nums[i] < 0)
                         {
                             nums[i] *= -1;
                         }
-                        product *= (nums[i]);
+                        product *= nums[i];
                     }
-
                     return product;
-
                 }
                 else if (nums.Length > 3)
-
                 {
                     for (int i = 0; i <= 2; i++)
                     {
-                        if ((nums[i]) < 0)
+                        if (nums[i] < 0)
                         {
                             nums[i] *= -1;
                         }
-                        product *= (nums[i]);
+                        product *= nums[i];
                     }
                     return product;
                 }
@@ -71,20 +85,198 @@
                 {
                     return 0;
                 }
-
                 else
                 {
                     return 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid input");
+                return 1;
+            }
+        }
+
+        public static double average()
+        {
+            double ave = 0;
+            try
+            {
+                Console.Write("input number between 2 and 10: ");
+                int size = Convert.ToInt32(Console.ReadLine());
+                if (size <= 2 || size >= 10)
+                {
+                    Console.Write("please enter number in the range!\n ");
+                    Console.WriteLine(average());
+                    return -1;
+                }
+
+                double[] array = new double[size];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write($" {i + 1} of {size} Enter a number:\t ");
+                    array[i] = Convert.ToDouble(Console.ReadLine());
+
+                    while (array[i] < 0)
+                    {
+                        array[i] *= -1;
+                    }
+                    ave += array[i];
 
                 }
+                ave /= size;
+
+                return Math.Floor(ave);
             }
             catch (FormatException ex)
             {
+                Console.WriteLine("please enter valid input");
+                average();
+                return 0;
 
-                Console.WriteLine("invaild input");
-                return 1;
             }
 
         }
+
+        static void Drawing()
+        {
+            int n = 5;
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int k = 1; k <= n - i; k++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int x = 1; x <= 2 * i - 1; x++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+
+            for (int i = n - 1; i >= 1; i--)
+            {
+                for (int k = 1; k <= n - i; k++) { Console.Write(" "); }
+
+
+                for (int x = 1; x <= 2 * i - 1; x++) { Console.Write("*"); }
+
+
+                Console.WriteLine();
+            }
+        }
+
+        public static int mostRebited(int[] array)
+        {
+            try
+            {
+                int most = 0;
+                int k = 0;
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    int x = 0;
+
+                    for (int j = 0; j < array.Length; j++)
+                    {
+                        if (array[i] == array[j])
+                        {
+                            x++;
+                        }
+                    }
+
+                    if (x > k)
+                    {
+                        k = x;
+                        most = array[i];
+                    }
+                }
+
+                return most;
+            }
+            catch (Exception e)
+            {
+                return 1;
+            }
+        }
+
+        public static int largerNumber(int[] num)
+        {
+            int max = num[0];
+            for (int i = 0; i < num.Length; i++)
+            {
+
+                if (num[i] > max)
+                {
+                    max = num[i];
+                }
+            }
+            return max;
+        }
+
+        public static void writeTofile(string sentences)
+        {
+            string path = "../../../words.txt";
+            File.AppendAllText(path, sentences);
+
+
+        }
+
+
+        public static void readFromfile()
+        {
+            string path = "../../../words.txt";
+            Console.WriteLine(File.ReadAllText(path));
+        }
+
+        public static void deleteFromfile(string sentences)
+        {
+            string path = "../../../words.txt";
+            string[] word = File.ReadAllLines(path);
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == sentences)
+                {
+                    word[i] = string.Empty;
+                }
+            }
+            File.WriteAllLines(path, word);
+        }
+
+        public static string[] senteneslength()
+        {
+            Console.WriteLine("Enter sentence: ");
+            string input = Console.ReadLine();
+            string[] words = input.Split(" ");
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = $"{words[i]}: {words[i].Length}";
+            }
+            return words;
+        }
+
+
+
+
     }
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
